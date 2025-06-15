8f16743ca4008bded0ff0809f8e36e090ac9b4fa
ferasraj <feras.raj@gmail.com>
2025-06-16 01:54:09 +0300

    refactor SideMenu component to use tailwind-merge for class management and improve radio button accessibility

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,5 +1,6 @@
 import { Link, useSearchParams } from "react-router-dom";
 import Search from "./Search";
+import { twMerge } from "tailwind-merge";
 
 const SideMenu = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -27,43 +28,67 @@ const SideMenu = () => {
       <Search />
       <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
       <div className="flex flex-col gap-2 text-sm">
-        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+        <label className="flex items-center gap-2 cursor-pointer">
           <input
             type="radio"
             name="sort"
             onChange={handleFilterChange}
             value="newest"
-            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+            className={twMerge(
+              "appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm ",
+              "bg-white checked:bg-blue-800"
+            )}
           />
-          Newest
+          <span>Newest</span>
         </label>
-        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+        <label
+          htmlFor="popular"
+          className="flex items-center gap-2 cursor-pointer"
+        >
           <input
+            id="popular"
             type="radio"
             name="sort"
             onChange={handleFilterChange}
             value="popular"
-            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+            className={twMerge(
+              "appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm ",
+              "bg-white checked:bg-blue-800"
+            )}
           />
           Most Popular
         </label>
-        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+        <label
+          htmlFor="trending"
+          className="flex items-center gap-2 cursor-pointer"
+        >
           <input
+            id="trending"
             type="radio"
             name="sort"
             onChange={handleFilterChange}
             value="trending"
-            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+            className={twMerge(
+              "appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm ",
+              "bg-white checked:bg-blue-800"
+            )}
           />
           Trending
         </label>
-        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+        <label
+          htmlFor="oldest"
+          className="flex items-center gap-2 cursor-pointer"
+        >
           <input
+            id="oldest"
             type="radio"
             name="sort"
             onChange={handleFilterChange}
             value="oldest"
-            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+            className={twMerge(
+              "appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm ",
+              "bg-white checked:bg-blue-800"
+            )}
           />
           Oldest
         </label>
